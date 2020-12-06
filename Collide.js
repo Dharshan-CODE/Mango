2dfcfe8505e53e9b6b0dbb320a08025c25dbafff
Dharshan-CODE <73929847+Dharshan-CODE@users.noreply.github.com>
2020-12-06 10:55:02 +0530

    Add files via upload

---
+++ Collide.js
@@ -0,0 +1,15 @@
+function Collide(lstone,lmango){
+
+  mangoBodyPosition=lmango.body.position
+  stoneBodyPosition=lstone.body.position
+  
+  var distance=dist(stoneBodyPosition.x, stoneBodyPosition.y, mangoBodyPosition.x, mangoBodyPosition.y)
+ 
+     console.log(lmango.r+lstone.r)
+  	if(distance<=lmango.r+lstone.r)
+    {
+     
+  	  Matter.Body.setStatic(lmango.body,false);
+    }
+
+  }
